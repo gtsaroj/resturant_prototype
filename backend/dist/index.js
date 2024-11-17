631d7ba51d01aa17243c8399dd75b130e0c5d861
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-11-17 12:47:44 +0545

    update:use routes

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import orderRouter from "./routes/message.routes.js";
+import webhookRouter from "./routes/webhook.routes.js";
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.listen(8000, () => {
-    console.log("Server started on port 8000.");
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
 });
+app.get("/test", (_, res) => {
+    res.send("Test route hit.");
+});
+app.use("/message", orderRouter);
+app.use("/fb", webhookRouter);
+export default app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import orderRouter from "./routes/message.routes.js";
+import webhookRouter from "./routes/webhook.routes.js";
 dotenv.config();
 
 const app = express();
@@ -9,7 +11,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-app.listen(8000, () => {
-  console.log(`Server started on port 8000.`);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
 });
+
+app.get("/test", (_, res) => {
+  res.send("Test route hit.");
+});
+app.use("/message", orderRouter);
+app.use("/fb", webhookRouter);
 export default app;
