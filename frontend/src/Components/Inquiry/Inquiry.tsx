e35534ba4668c01637b594b7ff224d9f9c993392
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-11-27 14:50:05 +0545

    remove: unused imports

--- frontend/src/Components/Inquiry/Inquiry.tsx
+++ frontend/src/Components/Inquiry/Inquiry.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { FaFacebookMessenger, FaWhatsapp, FaViber } from "react-icons/fa";
+import { FaFacebookMessenger, FaWhatsapp } from "react-icons/fa";
 import ReactLoading from "react-loading";
 import { useSuscribeFn } from "../../Hook/useSuscribe";
 
@@ -9,14 +9,14 @@ export const Inquiry = () => {
   const { title, button } = t("inquiry") as any;
 
   return (
-    <div className=" w-full lg:flex-row flex-col-reverse h-full py-10 flex items-start gap-10 ">
+    <div className="flex flex-col-reverse items-start w-full h-full gap-10 py-10  lg:flex-row">
       {/* Google Map iframe */}
       <div className="relative  w-full   lg:w-[55%] h-[250px] cursor-pointer lg:h-[400px] rounded-lg overflow-hidden">
         <div className="absolute  h-[100px] bottom-0 left-0   w-full  bg-gradient-to-t from-black to-transparent"></div>
 
         <iframe
           allowFullScreen={true}
-          className="w-full h-full rounded-lg transform transition-all duration-500 hover:scale-105"
+          className="w-full h-full transition-all duration-500 transform rounded-lg hover:scale-105"
           loading="lazy"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3026.897405829733!2d85.33158407477636!3d27.689059176193012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19e99b7e259f%3A0xe77e0b6b366f6c1c!2sDailo%20Daily%20See%20Us!5e1!3m2!1sen!2snp!4v1731576014026!5m2!1sen!2snp"
           title="Restaurant Location"
@@ -25,13 +25,13 @@ export const Inquiry = () => {
 
       {/* Inquiry Form Section */}
       <div className="w-full lg:w-[40%]  h-full   flex flex-col items-center justify-start gap-6 sm:gap-14 text-center">
-        <div className="w-full flex flex-col gap-9 items-center justify-center">
+        <div className="flex flex-col items-center justify-center w-full gap-9">
           <h1 className="sm:text-3xl text-2xl font-semibold tracking-wider text-[var(--primary-text)]">
             {title}
           </h1>
 
           {/* Social Media Links */}
-          <div className="flex gap-10 sm:gap-14 justify-center">
+          <div className="flex justify-center gap-10 sm:gap-14">
             <a
               target="_blank"
               className="bg-[#0078FF] p-4 rounded-full transition-colors duration-300 text-white hover:bg-[#0061D1] "
