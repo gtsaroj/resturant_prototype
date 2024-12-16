bdfbf8a21b3247e2fd6bf6d8cfdb65a895ed6769
gtsaroj <sarojgt326@gmail.com>
2024-12-16 18:25:32 +0545

    add swiper animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,12 +7,12 @@ export default {
   theme: {
     extend: {
       animation: {
-        marquee: 'marquee 10s linear infinite',
+        slide: "slide 10s linear infinite",
       },
       keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-100%)' },
+        slide: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
     },
